72009c57179a5eae0918b43b06ead9fff4668f2a
Neeraj Sharma <neerajs@microsoft.com>
2020-12-29 20:31:43 +0530

    rename docker hub image name

--- dotnet-images/build.cmd
+++ dotnet-images/build.cmd
@@ -1,1 +1,1 @@
-docker build -t neeraj9/dotnet-runtime-win:3.1.10-nanoserver-20H2 -f Dockerfile.dotnet-runtime-win_3.1.10-nanoserver-20H2 .
+docker build -t neeraj9/aspnetcore-runtime-win:3.1.10-nanoserver-20H2 -f Dockerfile.dotnet-runtime-win_3.1.10-nanoserver-20H2 .
